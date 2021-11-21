ddbd6118c163608852e9718f35b9b97c9515eb80
David Aikman <57766110+daikman@users.noreply.github.com>
2021-11-21 16:40:46 +0000

    add explanation and styling

--- image_1.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  imgCanvas1 = createCanvas(size, size);
-  imgCanvas1.parent('img_1')
-}
-
-function draw() {
-  image(img, 0, 0)
-}
--- image_2.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-  imgCanvas2 = createCanvas(size, size);
-  imgCanvas2.parent('img_2')
-}
-
-function draw() {
-  image(img2, 0, 0)
-}
